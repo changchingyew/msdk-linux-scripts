e51c076bdbeabd31267272d4afb1464a43dffa38
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-06-01 13:50:22 +0800

    Make script executable

--- msdk-close-download-compile.sh
+++ msdk-close-download-compile.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 set -x
 #Vaiables used
-# ipp_dir="ipp"
-# cmrt_dir="cmrt_linux"
+ipp_dir="ipp"
+cmrt_dir="cmrt_linux"
 
 # #Section to download source code
-# echo 'This script will compile mfx_transcoder, mfx_player, mfxhw64'
-# echo 'Enter your idsid to start with. e.g vsodhi'
-# read idsid
-# echo 'Downloading mdp_msdk-lib project'
-# echo 'git clone ssh://'$idsid'@git-ccr-1.devtools.intel.com:29418/mdp_msdk-lib'
-# git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-lib -b dev/iotg_vsi
-# echo 'Downloading mdp_msdk-contrib project'
-# echo 'git clone ssh://'$idsid'@git-ccr-1.devtools.intel.com:29418/mdp_msdk-contrib'
-# git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-contrib
-# echo 'git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-val-tools'
-# git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-val-tools
-# echo 'Downloading code base complete sucessfully'
-# sleep 2
-# echo
+echo 'This script will compile mfx_transcoder, mfx_player, mfxhw64'
+echo 'Enter your idsid to start with. e.g vsodhi'
+read idsid
+echo 'Downloading mdp_msdk-lib project'
+echo 'git clone ssh://'$idsid'@git-ccr-1.devtools.intel.com:29418/mdp_msdk-lib'
+git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-lib -b dev/iotg_vsi
+echo 'Downloading mdp_msdk-contrib project'
+echo 'git clone ssh://'$idsid'@git-ccr-1.devtools.intel.com:29418/mdp_msdk-contrib'
+git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-contrib
+echo 'git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-val-tools'
+git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-val-tools
+echo 'Downloading code base complete sucessfully'
+sleep 2
+echo
 
 # #Section to check for prerequisite
-# tar -xvf cmrt-ipp-deps-of-msdk.tar.gz
-# echo 'checking for dependencies needed'
-# if [ -d "$ipp_dir" ] && [ -d "$cmrt_dir" ] && [ -f "0001-fix-for-build-on-ubuntu.patch" ] ; then
-#   ### Take action if $DIR exists ###
-#   echo "Dependencies present can continue..."
-#   sleep 2
-# else
-#   echo "Error:  ipp and cmrt_linux folders not found. Can not continue compiling"
-#   exit 1
-# fi
-# echo
+tar -xvf cmrt-ipp-deps-of-msdk.tar.gz
+echo 'checking for dependencies needed'
+if [ -d "$ipp_dir" ] && [ -d "$cmrt_dir" ] && [ -f "0001-fix-for-build-on-ubuntu.patch" ] ; then
+  ### Take action if $DIR exists ###
+  echo "Dependencies present can continue..."
+  sleep 2
+else
+  echo "Error:  ipp and cmrt_linux folders not found. Can not continue compiling"
+  exit 1
+fi
+echo
 
 # #Secition to start code compiling
-# mv 0001-fix-for-build-on-ubuntu.patch mdp_msdk-lib
-# cd mdp_msdk-lib
-# git am 0001-fix-for-build-on-ubuntu.patch
-# cd ..
+mv 0001-fix-for-build-on-ubuntu.patch mdp_msdk-lib
+cd mdp_msdk-lib
+git am 0001-fix-for-build-on-ubuntu.patch
+cd ..
 echo 'Setting environment vaiables needed'
 export MEDIASDK_ROOT=`pwd`
 export MFX_HOME=$MEDIASDK_ROOT
@@ -63,11 +63,11 @@ echo
 echo
 # echo 'compiling mfx_player'
 # make -C __cmake/intel64.make.release -j 16 mfx_player
-# make -C __cmake/intel64.make.debug -j 16 mfx_player
+make -C __cmake/intel64.make.debug -j 16 mfx_player
 echo
 # echo 'compiling mfxhw64'
 # make -C __cmake/intel64.make.release -j 16 mfxhw64
-make -C __cmake/intel64.make.debug -j 16 mfxhw64
+# make -C __cmake/intel64.make.debug -j 16 mfxhw64
 
 echo 'compiling msdk_gmock'
 # make -C __cmake/intel64.make.debug -j 16 msdk_gmock

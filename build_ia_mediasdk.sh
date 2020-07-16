1664320c5ee8066eb1216bf92eb909ccd278a508
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-07-16 22:54:43 +0800

    Add VP9 build
    Signed-off-by: Yew, Chang Ching <chang.ching.yew@intel.com>

--- build_ia_mediasdk.sh
+++ build_ia_mediasdk.sh
@@ -5,5 +5,5 @@ set -x
 #cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_PLUGINS_CONF_DIR=/opt/intel/mediasdk/lib/mfx/ -DMFX_HW_KMB=ON -DAPI=latest -DENABLE_TESTS=ON
 #cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_PLUGINS_CONF_DIR=/opt/intel/mediasdk/lib/mfx/ -DMFX_HW_KMB=ON -DAPI=latest
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_KMB=ON -DAPI=latest
-make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 sample_encode sample_decode
+make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode
 #sudo make install

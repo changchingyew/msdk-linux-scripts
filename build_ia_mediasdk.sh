d0d7bbf772eaee1bddfe39bf08e4db6f4661052b
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-08-12 11:37:16 +0530

    Add kmb and tbh targets for MSDK build
    Signed-off-by: Yew, Chang Ching <chang.ching.yew@intel.com>

--- build_ia_mediasdk.sh
+++ build_ia_mediasdk.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-set -x
-#cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_TEXTLOG=true -DMFX_PLUGINS_CONF_DIR=/home/test/msdk/mdp_msdk-lib/build/__bin/Debug/ -DMFX_HW_KMB=ON -DAPI=latest
-#cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_PLUGINS_CONF_DIR=/opt/intel/mediasdk/lib/mfx/ -DMFX_HW_KMB=ON -DAPI=latest -DENABLE_TESTS=ON
-#cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_PLUGINS_CONF_DIR=/opt/intel/mediasdk/lib/mfx/ -DMFX_HW_KMB=ON -DAPI=latest
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_KMB=ON -DAPI=latest
-make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode
-#sudo make install
+#set -x
+if [ "$1" == "kmb" ]; then
+	echo "Build MediasSDK for $0"
+	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_KMB=ON -DAPI=latest
+	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
+elif [ "$1" == "tbh" ]; then
+	echo "Build MediasSDK for $0"
+	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_THB=ON -DAPI=latest
+	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 sample_encode sample_decode sample_multi_transcode
+else
+	echo "Usage: $0 kmb/tbh"
+fi

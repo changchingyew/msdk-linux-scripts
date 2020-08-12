da8417b024ebec41cae2ce6fd718d0668095b917
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-08-12 11:54:46 +0530

    Fix vp9 plugin for TBH MSDK build
    Signed-off-by: Yew, Chang Ching <chang.ching.yew@intel.com>

--- build_ia_mediasdk.sh
+++ build_ia_mediasdk.sh
@@ -4,11 +4,11 @@
 if [ "$1" == "kmb" ]; then
 	echo "Build MediasSDK for $0"
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_KMB=ON -DAPI=latest
-	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
+	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 sample_encode sample_decode sample_multi_transcode
 elif [ "$1" == "tbh" ]; then
 	echo "Build MediasSDK for $0"
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_THB=ON -DAPI=latest
-	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 sample_encode sample_decode sample_multi_transcode
+	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
 else
 	echo "Usage: $0 kmb/tbh"
 fi

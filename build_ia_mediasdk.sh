09c0a2886b7eb60e0ecd03a3148d643277eba0bc
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-09-03 07:29:50 +0530

    Add open source MSDK and vaapi shim HDDL builds
    Signed-off-by: Yew, Chang Ching <chang.ching.yew@intel.com>

--- build_ia_mediasdk.sh
+++ build_ia_mediasdk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #set -x
+
 if [ "$1" == "kmb" ]; then
 	echo "Build MediasSDK for $0"
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_KMB=ON -DAPI=latest
@@ -9,6 +10,9 @@ elif [ "$1" == "tbh" ]; then
 	echo "Build MediasSDK for $0"
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_THB=ON -DAPI=latest
 	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
+elif [ "$1" == "vsi" ]; then
+	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_VSI=ON -DMFX_VSI_HDDL=ON -DAPI=latest
+	make -j$(nproc) mfxhw64 mfx_hevcd_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
 else
-	echo "Usage: $0 kmb/tbh"
+	echo "Usage: $0 kmb/tbh/vsi"
 fi
--- build_ia_vaapi.sh
+++ build_ia_vaapi.sh
@@ -2,6 +2,8 @@
 set -x
 export SAFESTR_HOME=$PWD/../../safestringlib
 export CMAKE_PREFIX_PATH=$PWD/../../xlink/
-#cmake ..  -DUSE_HANTRO_DRIVER=ON -DTARGETS=IA -DDEBUG=ON
-cmake ..  -DUSE_HANTRO_DRIVER=ON -DTARGETS=IA
+export HDDLUNITE=1
+
+cmake ..  -DUSE_HANTRO_DRIVER=ON -DTARGETS=IA -DDEBUG=ON
+#cmake ..  -DUSE_HANTRO_DRIVER=ON -DTARGETS=IA
 make VERBOSE=1 -j$(nproc)

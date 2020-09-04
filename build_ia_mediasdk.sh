28291788a0cec13a2878435f557a233698ea6456
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-09-04 15:04:36 +0800

    Fix make target typo

--- build_ia_mediasdk.sh
+++ build_ia_mediasdk.sh
@@ -22,7 +22,7 @@ elif [ "$1" == "tbh" ]; then
 	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
 elif [ "$1" == "vsi" ]; then
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_VSI=ON $CMAKE_OPT -DAPI=latest
-	make -j$(nproc) mfxhw64 mfx_hevcd_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
+	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
 else
 	echo "Usage: $0 kmb/tbh/vsi [no_hddl]"
 fi

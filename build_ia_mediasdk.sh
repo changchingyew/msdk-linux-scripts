9a7b7936adf2c3b7ed22f97bb0a618597beca328
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-09-11 10:03:40 +0800

    Build open_source samples for tf codebase
    Signed-off-by: Yew, Chang Ching <chang.ching.yew@intel.com>

--- build_ia_mediasdk.sh
+++ build_ia_mediasdk.sh
@@ -16,10 +16,18 @@ if [ "$1" == "kmb" ]; then
 	echo "Build MediasSDK for $0"
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_KMB=ON $CMAKE_OPT -DAPI=latest
 	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 sample_encode sample_decode sample_multi_transcode
+
+	cd ../open_source/build
+	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_THB=ON $CMAKE_OPT -DAPI=latest
+	make -j$(nproc) sample_encode sample_decode sample_multi_transcode
 elif [ "$1" == "tbh" ]; then
 	echo "Build MediasSDK for $0"
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_THB=ON $CMAKE_OPT -DAPI=latest
 	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode
+
+	cd ../open_source/build
+	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_THB=ON $CMAKE_OPT -DAPI=latest
+	make -j$(nproc) sample_encode sample_decode sample_multi_transcode
 elif [ "$1" == "vsi" ]; then
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_VSI=ON $CMAKE_OPT -DAPI=latest
 	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode simple_decode_hddl  simple_transcode_hddl

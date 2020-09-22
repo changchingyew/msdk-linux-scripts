f3921c3a3e853211ee030aa9c4d6305e0b07c208
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-09-22 19:13:09 +0530

    Keep old option for vaapi build

--- build_ia_vaapi.sh
+++ build_ia_vaapi.sh
@@ -4,12 +4,12 @@ export SAFESTR_HOME=$PWD/../../safestringlib
 export CMAKE_PREFIX_PATH=$PWD/../../xlink/
 export HDDLUNITE=1
 
+echo "Running $0 $1"
 if [ "$1" == "kmb" ]; then
 	cmake ..  -DUSE_HANTRO_DRIVER=KMB -DTARGETS=IA -DDEBUG=ON
-	make VERBOSE=1 -j$(nproc)
 elif [ "$1" == "tbh" ]; then
 	cmake ..  -DUSE_HANTRO_DRIVER=TBH -DTARGETS=IA -DDEBUG=ON
-	make VERBOSE=1 -j$(nproc)
 else
-	echo "Usage: $0 kmb/tbh"
+	cmake ..  -DUSE_HANTRO_DRIVER=ON -DTARGETS=IA -DDEBUG=ON
 fi
+make VERBOSE=1 -j$(nproc)

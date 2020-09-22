b89411817c3fb85f40c6ef31b6796d77b285489d
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-09-22 19:06:01 +0530

    Add platform option for vaapi IA host build

--- build_ia_vaapi.sh
+++ build_ia_vaapi.sh
@@ -4,6 +4,12 @@ export SAFESTR_HOME=$PWD/../../safestringlib
 export CMAKE_PREFIX_PATH=$PWD/../../xlink/
 export HDDLUNITE=1
 
-cmake ..  -DUSE_HANTRO_DRIVER=ON -DTARGETS=IA -DDEBUG=ON
-#cmake ..  -DUSE_HANTRO_DRIVER=ON -DTARGETS=IA
-make VERBOSE=1 -j$(nproc)
+if [ "$1" == "kmb" ]; then
+	cmake ..  -DUSE_HANTRO_DRIVER=KMB -DTARGETS=IA -DDEBUG=ON
+	make VERBOSE=1 -j$(nproc)
+elif [ "$1" == "tbh" ]; then
+	cmake ..  -DUSE_HANTRO_DRIVER=TBH -DTARGETS=IA -DDEBUG=ON
+	make VERBOSE=1 -j$(nproc)
+else
+	echo "Usage: $0 kmb/tbh"
+fi

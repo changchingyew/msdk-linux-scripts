25c0722e90a0c95543e28793216f72bebeddc518
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-10-08 14:08:52 +0530

    MSDK gitlab build with oneVPL default option
    Signed-off-by: Yew, Chang Ching <chang.ching.yew@intel.com>

--- build_ia_mediasdk.sh
+++ build_ia_mediasdk.sh
@@ -3,13 +3,20 @@
 #set -x
 if [ "$2" == "no_hddl" ]; then
 	export CMAKE_OPT=
-else
+elif [ "$2" == "no_onevpl" ]; then
 	export HOST_INSTALL_DIR=/opt/intel/
 	export KMB_INSTALL_DIR=${HOST_INSTALL_DIR}/hddlunite
 	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${KMB_INSTALL_DIR}/lib
 	export PATH=$PATH:${KMB_INSTALL_DIR}/bin
 
 	export CMAKE_OPT=-DMFX_VSI_HDDL=ON
+else
+	export HOST_INSTALL_DIR=/opt/intel/
+	export KMB_INSTALL_DIR=${HOST_INSTALL_DIR}/hddlunite
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${KMB_INSTALL_DIR}/lib
+	export PATH=$PATH:${KMB_INSTALL_DIR}/bin
+
+	export CMAKE_OPT=-DMFX_VSI_HDDL=ON -DMFX_ONEVPL=ON
 fi
 
 if [ "$1" == "kmb" ]; then
@@ -29,12 +36,13 @@ elif [ "$1" == "tbh" ]; then
 	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_THB=ON $CMAKE_OPT -DAPI=latest
 	make -j$(nproc) sample_encode sample_decode sample_multi_transcode
 elif [ "$1" == "vsi" ]; then
-	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_VSI=ON $CMAKE_OPT -DAPI=latest
+	cmake .. -DCMAKE_BUILD_TYPE=Debug -DMFX_HW_VSI=ON $CMAKE_OPT -DAPI=latest -DENABLE_TEXTLOG=ON
 	make -j$(nproc) mfxhw64 mfx_hevce_hw64 mfx_hevcd_hw64 mfx_vp9d_hw64 sample_encode sample_decode sample_multi_transcode simple_decode_hddl  simple_transcode_hddl
 else
-	echo "Usage: $0 kmb/tbh/vsi [no_hddl]"
+	echo "Usage: $0 kmb/tbh/vsi [no_hddl/no_onevpl]"
 	echo -e "\tkmb for closed source kmb target"
 	echo -e "\ttbh for closed source tbh target"
 	echo -e "\tvsi for open source kmb/tbh target"
 	echo -e "\tno_hddl to disable HDDL unite build option"
+	echo -e "\tno_onevpl to disable oneVPL build option"
 fi

818c0889014589aa3ba231bc698f94dfb83adcb5
Yew, Chang Ching <chang.ching.yew@intel.com>
2020-11-24 07:26:50 +0530

    Update gstreamer build

--- build_ia_gst-plugins-good.sh
+++ build_ia_gst-plugins-good.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#./autogen.sh --disable-x11 --disable-wayland --disable-gtk-doc && make -j$(nproc)
+./autogen.sh --disable-x11 --disable-wayland --disable-gtk-doc && make -j$(nproc)
 sudo make install
--- build_ia_gstreamer-vaapi.sh
+++ build_ia_gstreamer-vaapi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./autogen.sh --disable-x11 --disable-wayland --disable-gtk-doc --enable-hantro
+./autogen.sh --disable-x11 --disable-wayland --disable-gtk-doc --enable-hantro --enable-tbh
 make -j$(nproc)
 sudo make install

19c9541fe961cabee6bfeece336aba63a2e90993
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-01-04 20:42:12 +0800

    Rename build_simics to native build_arm

--- build_arm_vsi_vaapi.sh
+++ build_arm_vsi_vaapi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -x
-export _PREFIX=/mnt/mnt/hdd2/cyew3/msdk/tbh/prefix
+export _PREFIX=/home/root/prefix
 export PKG_CONFIG_PATH=$_PREFIX/lib/pkgconfig
 export LIBVA_DRIVERS_PATH=$_PREFIX/lib
 export LD_LIBRARY_PATH=$_PREFIX/lib
+aclocal && autoconf && automake
 ./configure --disable-x11 --disable-wayland --enable-vsihw --enable-vsibo --prefix=$_PREFIX
 make -j $(nproc) all
 sudo make install

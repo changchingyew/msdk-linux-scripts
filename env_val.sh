51e6237890cd26784134cae431db14398af7a70c
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-01-08 09:58:49 +0800

    Remove /usr/local/local from validation LD_LIBRARY_PATH env

--- env_val.sh
+++ env_val.sh
@@ -7,4 +7,4 @@ export MEDIASDK_ROOT=$_MSDK_WORKDIR/validation_tool
 export MEDIASDK_STREAMS=$_MSDK_WORKDIR/streams
 
 export PATH=$_MSDK_WORKDIR/validation_tool/msdk_perl/perl-5.22.2/gcc2.14/lin_x64::$PATH
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH

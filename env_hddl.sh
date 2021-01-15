428bcbc9e75856434b2c212a386fcf5191a75558
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-01-15 10:43:26 +0800

    Fix env_hddl.sh

--- env_hddl.sh
+++ env_hddl.sh
@@ -3,6 +3,6 @@ DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
 
 source $DIR/env.sh
 
-export CONFIG_PATH=$DIR/connection_unite.cfg
+export CONFIG_PATH=$DIR/connection_hddl.cfg
 
 sudo hddl_scheduler_service &

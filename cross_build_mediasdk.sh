a06add69408ba1967e6609c08afe184abddfe9fd
Ravikanti, Venkatasainath <venkatasainath.ravikanti@intel.com>
2021-01-20 20:41:37 -0800

    Update cross_build_mediasdk.sh

--- cross_build_mediasdk.sh
+++ cross_build_mediasdk.sh
@@ -4,5 +4,5 @@ unset LD_LIBRARY_PATH
 source /usr/local/oecore-x86_64/environment-setup-aarch64-ese-linux
 
 cmake ../ -DMFX_HW_VSI_TARGET=ON -DMFX_VSI_HDDL=OFF -DAPI=latest -DT_ARCH_ARM=ON -DCMAKE_BUILD_TYPE=debug
-make -j$(nproc) mfxhw64 sample_decode sample_encode
+make -j$(nproc)
 

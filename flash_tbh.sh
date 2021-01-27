9b22fd8d049622f697d21b1af76a4d588eb0be68
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-01-27 12:47:49 +0800

    Update TBH flash script

--- flash_tbh.sh
+++ flash_tbh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-export TBH_SYSFS="/sys/devices/pci0000:00/0000:00:1d.0/0000:04:00.0/0000:05:08.0/0000:07:00.0"
+export TBH_SYSFS="/sys/devices/pci0000:00/0000:00:1b.0/0000:01:00.0/0000:02:08.0/0000:04:00.0"
 
 echo 1 | sudo tee $TBH_SYSFS/create_partitions
 
@@ -8,7 +8,7 @@ echo -n "system_a" | sudo tee $TBH_SYSFS/partition
 sudo dd if=system.img of=$TBH_SYSFS/image_data
 
 echo -n "boot_a" | sudo tee $TBH_SYSFS/partition
-sudo dd if=custom_boot.img of=$TBH_SYSFS/image_data
+sudo dd if=boot.img of=$TBH_SYSFS/image_data
 
 echo -n "syshash_a" | sudo tee $TBH_SYSFS/partition
 sudo dd if=syshash.img of=$TBH_SYSFS/image_data

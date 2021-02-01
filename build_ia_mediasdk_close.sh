56011d00c01f7b5cc573baf646464b46a7976e65
Ravikanti, Venkatasainath <venkatasainath.ravikanti@intel.com>
2021-01-31 22:55:47 -0800

    Update build_ia_mediasdk_close.sh with the new build procedure using iotg_vsi (of mdp_msdk-lib of 1source)

--- build_ia_mediasdk_close.sh
+++ build_ia_mediasdk_close.sh
@@ -2,9 +2,10 @@
 set -x
 
 echo 'Setting environment vaiables needed'
-export MAKE_TYPE=intel64.make.debug
+#export MAKE_TYPE=intel64.make.debug
 export MEDIASDK_ROOT=`pwd`
-export MFX_HOME=$MEDIASDK_ROOT
+#export MFX_HOME=$MEDIASDK_ROOT
+export MFX_HOME=$MEDIASDK_ROOT/mdp_msdk-lib
 export MFX_MDF_PATH=$MEDIASDK_ROOT/cmrt_linux
 export CMAKE_VTUNE_HOME=$MEDIASDK_ROOT/mdp_msdk-contrib/itt
 #export LIBVA_REV=${LIBVA_REV:-"va-unified"}
@@ -12,15 +13,22 @@ export CMAKE_VTUNE_HOME=$MEDIASDK_ROOT/mdp_msdk-contrib/itt
 #export PKG_CONFIG_TOP_BUILD_DIR=$PKG_CONFIG_PATH
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$LIBVA_ROOT
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:$LIBVA_ROOT
-ln -s mdp_msdk-lib/builder/CMakeLists.txt ./
+#ln -s mdp_msdk-lib/builder/CMakeLists.txt ./
 
-cd $MEDIASDK_ROOT
-perl mdp_msdk-lib/builder/build_mfx.pl --cmake=intel64.make.debug --no-warn-as-error
+#cd $MEDIASDK_ROOT
+cd $MFX_HOME
+mkdir build
+cd build
+#perl mdp_msdk-lib/builder/build_mfx.pl --cmake=intel64.make.debug --no-warn-as-error
+cmake .. -DCMAKE_BUILD_TYPE=debug
 
 # echo
-echo 'Compiling mfx_transcoder and player'
-make -C __cmake/$MAKE_TYPE -j 16 mfx_transcoder mfx_player
+#echo 'Compiling mfx_transcoder and player'
+#make -C __cmake/$MAKE_TYPE -j 16 mfx_transcoder mfx_player
 
-echo 'compiling msdk_gmock'
-make VERBOSE=1 -C __cmake/$MAKE_TYPE -j $(nproc) msdk_ts msdk_gmock
-echo 'Compilation successful, you can find bin generated in ./__cmake/intel64.make.release/__bin/release/'
+#echo 'compiling msdk_gmock'
+echo 'Compiling all the validaitons tools and apps'
+#make VERBOSE=1 -C __cmake/$MAKE_TYPE -j $(nproc) msdk_ts msdk_gmock
+make -j$(nproc)
+#echo 'Compilation successful, you can find bin generated in ./__cmake/intel64.make.release/__bin/release/'
+echo 'Compilation successful!!'

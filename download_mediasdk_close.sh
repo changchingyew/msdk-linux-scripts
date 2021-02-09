089617931d4a4ca6386506e4a0a3e5f6c0bd15ba
Ravikanti, Venkatasainath <venkatasainath.ravikanti@intel.com>
2021-02-08 18:34:04 -0800

    Changing the repo links from TeamForge to 1Source GitHub

--- download_mediasdk_close.sh
+++ download_mediasdk_close.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 set -x
 #Vaiables used
-ipp_dir="ipp"
-cmrt_dir="cmrt_linux"
+#ipp_dir="ipp"
+#cmrt_dir="cmrt_linux"
 
  #Section to download source code
 echo 'This script will compile mfx_transcoder, mfx_player, mfxhw64'
-echo 'Enter your idsid to start with. e.g vsodhi'
-read idsid
+#echo 'Enter your idsid to start with. e.g vsodhi'
+#read idsid
 echo 'Downloading mdp_msdk-lib project'
-echo 'git clone ssh://'$idsid'@git-ccr-1.devtools.intel.com:29418/mdp_msdk-lib'
-git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-lib -b dev/iotg_vsi
+echo 'git clone https://github.com/intel-innersource/drivers.gpu.media.sdk.lib.git'
+git clone https://github.com/intel-innersource/drivers.gpu.media.sdk.lib.git -b iotg_vsi
+echo
+
 echo 'Downloading mdp_msdk-contrib project'
-echo 'git clone ssh://'$idsid'@git-ccr-1.devtools.intel.com:29418/mdp_msdk-contrib'
-git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-contrib
-echo 'git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-val-tools'
-git clone ssh://$idsid@git-ccr-1.devtools.intel.com:29418/mdp_msdk-val-tools
+echo 'git clone https://github.com/intel-innersource/drivers.gpu.media.sdk.contrib.git'
+git clone https://github.com/intel-innersource/drivers.gpu.media.sdk.contrib.git
+echo
+
+echo 'Downloading mdp_msdk-val-tools project'
+echo 'git clone https://github.com/intel-innersource/drivers.gpu.media.sdk.val-tools.git'
+git clone https://github.com/intel-innersource/drivers.gpu.media.sdk.val-tools.git
+echo
+
 echo 'Downloading code base complete sucessfully'
 sleep 2
 echo
 
 #Section to check for prerequisite
-tar -xvf cmrt-ipp-deps-of-msdk.tar.gz
-echo 'checking for dependencies needed'
-if [ -d "$ipp_dir" ] && [ -d "$cmrt_dir" ] && [ -f "0001-fix-for-build-on-ubuntu.patch" ] ; then
+#tar -xvf cmrt-ipp-deps-of-msdk.tar.gz
+#echo 'checking for dependencies needed'
+#if [ -d "$ipp_dir" ] && [ -d "$cmrt_dir" ] && [ -f "0001-fix-for-build-on-ubuntu.patch" ] ; then
   ### Take action if $DIR exists ###
-  echo "Dependencies present can continue..."
-  sleep 2
-else
-  echo "Error:  ipp and cmrt_linux folders not found. Can not continue compiling"
-  exit 1
-fi
-echo
+#  echo "Dependencies present can continue..."
+#  sleep 2
+#else
+#  echo "Error:  ipp and cmrt_linux folders not found. Can not continue compiling"
+#  exit 1
+#fi
+#echo

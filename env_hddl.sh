8a80274fdf0997e44f6c049743d23b616e6614e5
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-05-04 16:34:53 +0800

    Update env_hddl.sh for tbh beta2 bkc

--- env_hddl.sh
+++ env_hddl.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
-
-source $DIR/env.sh
+export MFX_HOME=/opt/intel/onevpl
+export PATH=$MFX_HOME/bin:$MFX_HOME/share/mfx/samples/_bin:$PATH
+export LD_LIBRARY_PATH=$MFX_HOME/lib64:$MFX_HOME/lib64/mfx:$LD_LIBRARY_PATH
 
 export CONFIG_PATH=$DIR/connection_hddl.cfg
-
-sudo /opt/intel/hddlunite/bin/hddl_scheduler_service &
-
-sleep 3
-/opt/intel/hddlunite/bin/SetHDDLMode -m b

39099aff32ae4d7ddaed4221aaed10170f343943
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-06-14 09:14:35 +0800

    Simplify python scripts

--- full_test_gst.py
+++ full_test_gst.py
@@ -4,8 +4,7 @@ import os
 with open('GST-MSDK_Testcases.csv') as f:
     reader = csv.reader(f)
     for csv_row in reader:
-        exec_cmds=csv_row[21]
-        exec_cmd_array=exec_cmds.splitlines()
+        exec_cmd_array=csv_row[21].splitlines()
         exec_cmd_array_len = len(exec_cmd_array)
         if ( exec_cmd_array_len==1 ):
             exec_cmd_test=exec_cmd_array[0]
@@ -13,7 +12,6 @@ with open('GST-MSDK_Testcases.csv') as f:
                 print(exec_cmd_test)
                 #os.system(exec_cmd_test)
         elif ( exec_cmd_array_len==2 or exec_cmd_array_len==3 ):
-            exec_cmd_test_line=exec_cmd_array[1]
-            exec_cmd_test=exec_cmd_test_line[2:]
+            exec_cmd_test=exec_cmd_array[1][2:]
             print(exec_cmd_test)
             #os.system(exec_cmd_test)

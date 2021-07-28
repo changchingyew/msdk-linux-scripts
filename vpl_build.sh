2354cca3dec37aead5bf5fa6218fe3deb12f9b88
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-07-28 09:54:34 +0000

    Update vpl build scripts

--- vpl_build.sh
+++ vpl_build.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 set -x
+#cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Debug
 cmake .. -DCMAKE_INSTALL_PREFIX=/home/root/vpl_root/ -DCMAKE_BUILD_TYPE=Debug
-#cmake --build . --config Debug --target install
 cmake --build . --config Debug --verbose
+#cmake --build . --config Debug --target install

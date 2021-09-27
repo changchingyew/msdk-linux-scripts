cb61d2869a9cf7c2613450e652e66d90735d75e6
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-09-27 22:36:46 +0000

    Rename transcode scripts

--- ia_media_transcode.sh
+++ ia_media_transcode.sh
@@ -1,1 +1,12 @@
-/usr/bin/sample_multi_transcode -hw -i::h264 /media/H264_Videos/Demuxed_H264_Videos/Puppies_1920x1080_40mbps_30fps_Main_at_L4.1.h264 -o::h265 /tmp/output.265 -w 1920 -h 1080 -b 8000 -f 30
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]:-$0}")" >/dev/null 2>&1 ; pwd -P )"
+. "${SCRIPT_DIR}/ia_media_env.sh" 
+
+PAR_FILE_PATH=./msdk-linux-scripts/par_files
+#PAR_FILE=incremental_decode_4_X.par
+#PAR_FILE=decode_4_postproc_X.par
+#PAR_FILE=decode_4_postproc_X2.par
+PAR_FILE=dec_16_comp.par
+
+${SAMPLE_PATH}/sample_multi_transcode -par ${PAR_FILE_PATH}/${PAR_FILE} 
---
+++ ia_media_transcode_one.sh
@@ -0,0 +1,1 @@
+/usr/bin/sample_multi_transcode -hw -i::h264 /media/H264_Videos/Demuxed_H264_Videos/Puppies_1920x1080_40mbps_30fps_Main_at_L4.1.h264 -o::h265 /tmp/output.265 -w 1920 -h 1080 -b 8000 -f 30

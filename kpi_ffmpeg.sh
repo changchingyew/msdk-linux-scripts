3133ddd145580423c85c3b717cfcf1ae9170defc
Yew, Chang Ching <chang.ching.yew@intel.com>
2021-10-26 13:23:45 +0000

    Add JC's kpi scripts

---
+++ kpi_ffmpeg.sh
@@ -0,0 +1,181 @@
+H264
+
+#Decode 1080p 32x
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode1.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode2.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode3.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode4.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode5.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode6.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode7.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode8.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode9.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode10.log &
+
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode11.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode12.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode13.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode14.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode15.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode16.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode17.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode18.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode19.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode20.log &
+
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode21.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode22.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode23.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode24.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode25.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode26.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode27.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode28.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode29.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode30.log &
+
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode31.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i Fireworks_1920x1080_12mbps_60fps_Baseline_at_L4.2-vince.h264 -f null - 2> out/1080_decode32.log &
+
+
+
+#Decode: 4K 6x.
+ffmpeg -hwaccel qsv -c:v h264_qsv -i /home/tgljc/vince/Puppies_3840x2160_20mbps_60fps_High_at_L5.2-vince.h264 -f null - 2> out/4K_decode1.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i /home/tgljc/vince/Puppies_3840x2160_20mbps_60fps_High_at_L5.2-vince.h264 -f null - 2> out/4K_decode2.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i /home/tgljc/vince/Puppies_3840x2160_20mbps_60fps_High_at_L5.2-vince.h264 -f null - 2> out/4K_decode3.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i /home/tgljc/vince/Puppies_3840x2160_20mbps_60fps_High_at_L5.2-vince.h264 -f null - 2> out/4K_decode4.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i /home/tgljc/vince/Puppies_3840x2160_20mbps_60fps_High_at_L5.2-vince.h264 -f null - 2> out/4K_decode5.log &
+ffmpeg -hwaccel qsv -c:v h264_qsv -i /home/tgljc/vince/Puppies_3840x2160_20mbps_60fps_High_at_L5.2-vince.h264 -f null - 2> out/4K_decode6.log &
+
+
+
+
+
+#Encode: 4K x4
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 3840x2160 -i /home/tgljc/media-profiling-tool/media-content/RAW/Puppies_3840x2160_20mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 50M -maxrate 100M -f null - 2> out/4K_encode1.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 3840x2160 -i /home/tgljc/media-profiling-tool/media-content/RAW/Puppies_3840x2160_20mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 50M -maxrate 100M -f null - 2> out/4K_encode2.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 3840x2160 -i /home/tgljc/media-profiling-tool/media-content/RAW/Puppies_3840x2160_20mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 50M -maxrate 100M -f null - 2> out/4K_encode3.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 3840x2160 -i /home/tgljc/media-profiling-tool/media-content/RAW/Puppies_3840x2160_20mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 50M -maxrate 100M -f null - 2> out/4K_encode4.log &
+
+
+#Encode: 1080p x4
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i /home/tgljc/media-profiling-tool/media-content/RAW/Fireworks_1920x1080_12mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 20M -maxrate 20M -f null - 2> out/1080_encode1.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i /home/tgljc/media-profiling-tool/media-content/RAW/Fireworks_1920x1080_12mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 20M -maxrate 20M -f null - 2> out/1080_encode2.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i /home/tgljc/media-profiling-tool/media-content/RAW/Fireworks_1920x1080_12mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 20M -maxrate 20M -f null - 2> out/1080_encode3.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i /home/tgljc/media-profiling-tool/media-content/RAW/Fireworks_1920x1080_12mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 20M -maxrate 20M -f null - 2> out/1080_encode4.log &
+
+
+#encode: 720p x1
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1280x720 -i /home/tgljc/media-profiling-tool/media-content/RAW/Cactus_1280x720_9mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 14M -maxrate 14M -f null - 2> out/720_encode1.log &
+
+
+########################################################################
+# Note: Termination codes. 
+sleep 3m
+echo "Terminate App"
+killall -2 ffmpeg
+
+########################################################################
+# Note: wait for ffmpeg process are killed. This to time overall ffmpeg
+#       aggregate usage. 
+#
+#echo "going to wait"
+
+#while true;
+#do
+    #NUM=`ps -aux | grep -i ffmpeg | wc -l`
+    #if [ $NUM -eq 1 ];then
+       #break;
+    #fi
+#done
+#echo "wait done"
+
+
+H265
+
+#Decode 1080p 32x
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode1.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode2.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode3.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode4.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode5.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode6.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode7.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode8.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode9.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode10.log &
+
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode11.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode12.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode13.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode14.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode15.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode16.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode17.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode18.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode19.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode20.log &
+
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode21.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode22.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode23.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode24.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode25.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode26.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode27.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode28.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode29.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode30.log &
+
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode31.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Duck_1920x1080_3mbps_25fps_Main_at_L4.0_10bit-vince.h265 -f null - 2> out/1080_decode32.log &
+
+#Decode: 4K 6x.
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Bunny_3840x2160_15mbps_60fps_Main_at_L5.1-vince.265 -f null - 2> out/4K_decode1.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Bunny_3840x2160_15mbps_60fps_Main_at_L5.1-vince.265 -f null - 2> out/4K_decode2.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Bunny_3840x2160_15mbps_60fps_Main_at_L5.1-vince.265 -f null - 2> out/4K_decode3.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Bunny_3840x2160_15mbps_60fps_Main_at_L5.1-vince.265 -f null - 2> out/4K_decode4.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Bunny_3840x2160_15mbps_60fps_Main_at_L5.1-vince.265 -f null - 2> out/4K_decode5.log &
+ffmpeg -hwaccel qsv -c:v hevc_qsv -i /home/tgljc/vince/Bunny_3840x2160_15mbps_60fps_Main_at_L5.1-vince.265 -f null - 2> out/4K_decode6.log &
+
+
+
+
+#Encode: 4K x4
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 3840x2160 -i /home/tgljc/media-profiling-tool/media-content/RAW/Puppies_3840x2160_20mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 25M -maxrate 25M -f null - 2> out/4K_encode1.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 3840x2160 -i /home/tgljc/media-profiling-tool/media-content/RAW/Puppies_3840x2160_20mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 25M -maxrate 25M -f null - 2> out/4K_encode2.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 3840x2160 -i /home/tgljc/media-profiling-tool/media-content/RAW/Puppies_3840x2160_20mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 25M -maxrate 25M -f null - 2> out/4K_encode3.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 3840x2160 -i /home/tgljc/media-profiling-tool/media-content/RAW/Puppies_3840x2160_20mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 25M -maxrate 25M -f null - 2> out/4K_encode4.log &
+
+
+#Encode: 1080p x4
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i /home/tgljc/media-profiling-tool/media-content/RAW/Fireworks_1920x1080_12mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 12M -maxrate 12M -f null - 2> out/1080_encode1.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i /home/tgljc/media-profiling-tool/media-content/RAW/Fireworks_1920x1080_12mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 12M -maxrate 12M -f null - 2> out/1080_encode2.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i /home/tgljc/media-profiling-tool/media-content/RAW/Fireworks_1920x1080_12mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 12M -maxrate 12M -f null - 2> out/1080_encode3.log &
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i /home/tgljc/media-profiling-tool/media-content/RAW/Fireworks_1920x1080_12mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 12M -maxrate 12M -f null - 2> out/1080_encode4.log &
+
+
+#encode: 720p x1
+ffmpeg -stream_loop -1 -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1280x720 -i /home/tgljc/media-profiling-tool/media-content/RAW/Cactus_1280x720_9mbps_60fps.nv12 -vf hwupload=extra_hw_frames=64,format=qsv -c:v hevc_qsv -b:v 10M -maxrate 10M -f null - 2> out/720_encode1.log &
+
+
+########################################################################
+# Note: Termination codes. 
+sleep 3m
+echo "Terminate App"
+killall -2 ffmpeg
+
+########################################################################
+# Note: wait for ffmpeg process are killed. This to time overall ffmpeg
+#       aggregate usage. 
+#
+#echo "going to wait"
+
+#while true;
+#do
+    #NUM=`ps -aux | grep -i ffmpeg | wc -l`
+    #if [ $NUM -eq 1 ];then
+       #break;
+    #fi
+#done
+#echo "wait done"

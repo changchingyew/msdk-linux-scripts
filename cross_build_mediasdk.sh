36267aa4323e51fa325aef6bc5872cf1b15ef655
Yew, Chang Ching <chang.ching.yew@intel.com>
2022-03-14 21:21:53 +0800

    Cross build msdk only

--- cross_build_mediasdk.sh
+++ cross_build_mediasdk.sh
@@ -3,5 +3,6 @@ set -x
 unset LD_LIBRARY_PATH
 source /usr/local/oecore-x86_64/environment-setup-aarch64-ese-linux
 
-cmake ../ -DMFX_HW_VSI=ON -DMFX_VSI_HDDL=OFF -DAPI=latest -DT_ARCH_ARM=ON -DCMAKE_BUILD_TYPE=debug -DMFX_ONEVPL=ON
+#cmake ../ -DMFX_HW_VSI=ON -DMFX_VSI_HDDL=OFF -DAPI=latest -DT_ARCH_ARM=ON -DCMAKE_BUILD_TYPE=debug -DMFX_ONEVPL=ON
+cmake ../ -DMFX_HW_VSI=ON -DMFX_VSI_HDDL=OFF -DAPI=latest -DT_ARCH_ARM=ON -DCMAKE_BUILD_TYPE=release
 make -j$(nproc)

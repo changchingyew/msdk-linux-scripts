f5761e0a305dd7c52df3e3f6eaa037271b1c7316
Yew, Chang Ching <chang.ching.yew@intel.com>
2022-03-18 14:17:11 +0800

    Extract elapse time awk script into file

---
+++ perf_elapsed.awk
@@ -0,0 +1,2 @@
+NR == 1{split($4, t, /:/); old = t[1]; next}     # if 1st line
+{split($4, t, /:/); elapsed=(t[1] - old)*1000000 ; print elapsed, " usec"; old = t[1]}  # else...' perf_script.txt > elapsed_time.txt
--- perf_func_elapsed.sh
+++ perf_func_elapsed.sh
@@ -1,5 +1,4 @@
 set -x
 perf script > perf_script.txt
-awk ' NR == 1{split($4, t, /:/); old = t[1]; next}     # if 1st line
-{split($4, t, /:/); elapsed=(t[1] - old)*1000000 ; print elapsed, " usec"; old = t[1]}  # else...' perf_script.txt > elapsed_time.txt
-#perf probe -d probe_libmfxhw64:*
+grep sample_multi_tr perf_script.txt | awk -f perf_elapsed.awk > elapsed_time.txt
+perf probe -d probe_libmfxhw64:*
